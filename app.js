9077aa2258d3f11b429951689589c621e5c29acb
Matheus Rodrigues <62529344+contrastguy@users.noreply.github.com>
2022-05-19 23:21:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-fetch('https://aws.random.cat/meow')
-.then(function(response) {
-console.log(response);
-  response.json()
-  .then(function(convertido){
-        console.log(convertido);
+fetch('https://aws.random.cat/meow') //pega o link da API com o fetch 
+.then(function(response) {   //.then(uma função para pegar a resposta da sua request da API (parâmetro pra usar no escopo da função, com signicado de ser a resposta da request){})
+console.log(response);   // pra mostrar a resposta da request da API
+  response.json()     // transforma essa resposta em um JSON
+  .then(function(convertido){   //esse JSON é uma promisse, logo , usa .then( função com o parâmetro transformado em JSON(){})
+        console.log(convertido);  // Joga esse console.log para tirar de uma promisse pendente , e aparecer os dados transformados em JSON.
   });
 });
+
+
+
+const url = "developer.marvel.com"
+
+fetch(url).then(function(resposta){
+  console.log(resposta);
+    resposta.json()
+    .then(function(dadosConvertidos){
+      console.log(dadosConvertidos);
+    })
+})
